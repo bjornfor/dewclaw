a3019f399a6078f128310d85355c2c0c0263097c
Bjørn Forsman <bjorn.forsman@gmail.com>
2025-12-19 20:55:12 +0100

    Add OpenWRT 24.10 support
    The latest OpenWRT release replaced the opkg package manager with apk.
    Add support for apk while keeping opkg support for backward
    compatibility with OpenWRT 23.05.
    
    Implementation notes:
    
    * apk doesn't like dots in package names. Since the word "deps" is used
      a lot in the Nix and sh code I changed the OpenWRT package name from
      ".extra-system-deps." to "dewclaw-deps".
    * To create the apk package file I'm using `apk mkpkg`. That's a newer
      command, but much simpler to use than `abuild`. Bump the `apk-tools`
      version in the overlay to get a working version, if the input version
      is too old. Also, see https://github.com/NixOS/nixpkgs/pull/472471.
    
    TODO: The current package is unsigned. Should we sign it?

--- default.nix
+++ default.nix
@@ -1,7 +1,28 @@
 {
   pkgs ? import <nixpkgs> {
     config = { };
-    overlays = [ ];
+    overlays = [
+      (final: prev: {
+        # Upgrade apk-tools if needed.
+        apk-tools =
+          if prev.lib.versionOlder prev.apk-tools.version "3.0" then
+            prev.apk-tools.overrideAttrs (old: {
+              version = "3.0.3";
+              src = prev.fetchFromGitLab {
+                domain = "gitlab.alpinelinux.org";
+                owner = "alpine";
+                repo = "apk-tools";
+                rev = "v3.0.3";
+                sha256 = "sha256-ydqJiLkz80TQGyf9m/l8HSXfoTAvi0av7LHETk1c0GI=";
+              };
+              buildInputs = (old.buildInputs or []) ++ [
+                prev.zstd
+              ];
+            })
+          else
+            prev.apk-tools;
+      })
+    ];
   },
   configuration,
 }:
